7cf20abb45675db256ec0b9a30d498506dd0b9f0
faizanchattha577 <faizanchattha57@gmail.com>
2021-12-31 01:17:27 +0500

    second commit

--- index.js
+++ index.js
@@ -6,13 +6,17 @@ import hostelApplications from "./Routes/hostelApplication.js"
 
 const app = express();
 
-const url =
-  "mongodb://localhost/students";
+// const url =
+//   "mongodb://localhost/students";
 
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true} )
-.then(() => console.log("Connected to the database."));
+// mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true} )
+// .then(() => console.log("Connected to the database."));
 
-app.listen(5000);
+// app.listen(5000);
+const url = "mongodb+srv://faizan:1234@cluster0.wptzy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log("connected to the database."))
+    .catch(err => console.log(err));
 
 app.use(cors());
 app.use(bodyParser.json( { extended: true } ));
